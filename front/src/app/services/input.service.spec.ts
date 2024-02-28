e90e4bc598c88f1b11d48212aa3338d95f5d8032
770menahem <mnjol770@gmail.com>
2024-02-28 11:19:28 +0200

    front: add env

--- front/src/app/services/input.service.spec.ts
+++ front/src/app/services/input.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { InputService } from './input.service';
+import { environment } from '../../environments/environment.development';
 
 describe('InputService', () => {
-  let service: InputService;
+    let service: InputService;
+    let httpTestingController: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(InputService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+        });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+        service = TestBed.inject(InputService);
+        httpTestingController = TestBed.inject(HttpTestingController);
+    });
+
+    afterEach(() => {
+        httpTestingController.verify();
+    });
+
+    it('should make API call and return cities', () => {
+        const mockCities: any[] = [
+            {
+                name: 'Chicago',
+            },
+        ];
+
+        service.getCities('chicago').subscribe((cities) => {
+            expect((cities as string[]).length).toBe(1);
+            expect(cities).toEqual(mockCities);
+        });
+
+        const req = httpTestingController.expectOne(`${environment.backend_url}/cities?term=chicago`);
+
+        expect(req.request.method).toEqual('GET');
+
+        req.flush(mockCities);
+    });
 });
--- front/src/app/services/input.service.ts
+++ front/src/app/services/input.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
     providedIn: 'root',
@@ -8,6 +9,6 @@ export class InputService {
     constructor(private http: HttpClient) {}
 
     getCities(input: string) {
-        return this.http.get(`http://localhost:1370/api/city/search?city=${input}`);
+        return this.http.get(`${environment.backend_url}/city/search?city=${input}`);
     }
 }
