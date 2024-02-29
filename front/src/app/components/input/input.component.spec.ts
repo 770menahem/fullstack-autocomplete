9e0a884cd7a4c363df9f4524f8d434d22f291d53
770menahem <mnjol770@gmail.com>
2024-02-29 15:26:04 +0200

    front: upgrade test

--- front/src/app/components/input/input.component.spec.ts
+++ front/src/app/components/input/input.component.spec.ts
@@ -27,27 +27,22 @@ describe('InputComponent', () => {
     it('should not show cities when input short', async () => {
         const userInput = 't';
 
-        component.cities = mockCities.map((city) => city.name);
-        component.error = '';
-        component.showOptions = true;
-        component.input.setValue(userInput);
+        spyOn(inputService, 'getCities').and.returnValue(of(mockCities));
+
+        const input = fixture.nativeElement.querySelector('input');
+        input.value = userInput;
+        input.dispatchEvent(new Event('input'));
 
         fixture.detectChanges();
         await fixture.whenStable();
 
         const ul = fixture.nativeElement.querySelector('ul');
         expect(ul).toBeFalsy();
-
-        const li = fixture.nativeElement.querySelectorAll('li');
-
-        expect(li.length).toBe(0);
     });
 
     it('should not get cities below 2 characters', async () => {
         const userInput = 't';
         spyOn(inputService, 'getCities').and.returnValue(of(mockCities));
-
-        fixture.detectChanges();
 
         const input = fixture.nativeElement.querySelector('input');
         input.value = userInput;
@@ -57,7 +52,6 @@ describe('InputComponent', () => {
         await fixture.whenStable();
 
         expect(component.showOptions).toBe(false);
-
         expect(component.cities).toEqual([]);
         expect(component.error).toBe('');
     });
@@ -99,15 +93,16 @@ describe('InputComponent', () => {
 
     it('should show "not found" when input valid and no cities', async () => {
         const userInput = 'tel';
-        const cities: { name: string }[] = [];
-
-        component.cities = cities.map((city) => city.name);
-        component.error = '';
-        component.showOptions = true;
-        component.input.setValue(userInput);
+        spyOn(inputService, 'getCities').and.returnValue(of([]));
 
         fixture.detectChanges();
+
+        const input = fixture.nativeElement.querySelector('input');
+        input.value = userInput;
+        input.dispatchEvent(new Event('input'));
+
         await fixture.whenStable();
+        fixture.detectChanges();
 
         const ul = fixture.nativeElement.querySelector('ul');
         expect(ul).toBeTruthy();
@@ -120,14 +115,16 @@ describe('InputComponent', () => {
 
     it('should show cities when cities and input are valid', async () => {
         const userInput = 'tel';
-
-        component.cities = mockCities.map((city) => city.name);
-        component.error = '';
-        component.showOptions = true;
-        component.input.setValue(userInput);
+        spyOn(inputService, 'getCities').and.returnValue(of(mockCities));
 
         fixture.detectChanges();
+
+        const input = fixture.nativeElement.querySelector('input');
+        input.value = userInput;
+        input.dispatchEvent(new Event('input'));
+
         await fixture.whenStable();
+        fixture.detectChanges();
 
         const ul = fixture.nativeElement.querySelector('ul');
         expect(ul).toBeTruthy();
